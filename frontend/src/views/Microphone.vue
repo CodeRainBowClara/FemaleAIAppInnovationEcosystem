5ddf66bd558ac5c69752fe4bb90204e6b61a16c1
Clara <102231956+CodeRainBowClara@users.noreply.github.com>
2022-03-24 16:51:24 +0100

    Update Microphone.vue

--- frontend/src/views/Microphone.vue
+++ frontend/src/views/Microphone.vue
@@ -5,6 +5,8 @@
             <select name="lang" @change="onChange($event)" class="custom-select">
                 <option value="de-DE" selected>German</option>
                 <option value="en-US">English</option>
+                <option value="ko-KR">Korean</option>
+                <option value="ro-RO">Rumänisch</option>
             </select>
         </div>
         <br>
